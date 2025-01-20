b1250d20831e176e3f7c5e10d38998f584fe72ca
Nestor Suarez Delgado <nestorsdelgado@gmail.com>
2025-01-20 16:18:18 +0000

    Day 1
    Co-authored-by: Gara Luzardo <garaluzardo@users.noreply.github.com>

--- src/quiz.js
+++ src/quiz.js
@@ -3,13 +3,52 @@ class Quiz {
     //
     // 1. constructor (questions, timeLimit, timeRemaining)
 
-    // 2. getQuestion()
+    constructor(questions, timeLimit, timeRemaining) {
+
+        this.questions = questions
+        this.timeLimit = timeLimit
+        this.timeRemaining = timeRemaining
+        this.correctAnswers = 0
+        this.currentQuestionIndex = 0
+    }
+
+    // Methods
     
-    // 3. moveToNextQuestion()
+    getQuestion() {
 
-    // 4. shuffleQuestions()
+        return this.questions[this.currentQuestionIndex]
+    }
 
-    // 5. checkAnswer(answer)
+    moveToNextQuestion() {
 
-    // 6. hasEnded()
+        this.currentQuestionIndex++
+    }
+
+    shuffleQuestions(){
+
+        for (let i = this.questions.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [this.questions[i], this.questions[j]] = [this.questions[j], this.questions[i]];
+        }
+    }
+
+    checkAnswer(answer){
+
+        // questions -> array de Questions(la otra clase) -> cuyo tercer index es la respuesta
+        // si answer = correcta
+
+        if(this.questions.answer == this.answer) {
+            this.correctAnswers++
+        }
+    }
+
+    hasEnded(){
+
+        if(this.currentQuestionIndex < this.questions.length) {
+            return false
+        } else {
+            return true
+        }
+    }
+
 }
